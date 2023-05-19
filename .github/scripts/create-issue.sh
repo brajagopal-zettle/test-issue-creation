9b5aeee55d20d541ab058fe56c91f153917221a6
Balaji Rajagopal <brajagopal@paypal.com>
2023-05-19 16:47:26 +0200

    Added steps in create issue

--- .github/scripts/create-issue.sh
+++ .github/scripts/create-issue.sh
@@ -3,6 +3,24 @@
 export PRE_RELEASE_TITLE="[deploy] Release v"
 export CLOSE_ISSUE_COMMENT="I'm closing this issue to create a new one."
 
+issue_steps=$(cat <<-EOM
+<br/><br/>
+## Steps
+
+- [ ] Wait for sign-off by all contributors
+- [ ] Start `prod` deployment by publishing the `prerelease` Release tag to trigger the CirceCI deployment.
+- [ ] Monitor the logs in splunk
+- [ ] Post text descriptions (not the raw commit log), including a link to this issue to Slack #release-info
+- [ ] Close this Issue
+
+## Abort
+
+In case of any issue with `prod` deployment or want to abort this release process, please close this issue to unlock the master branch
+so that further pull request can be merged.
+
+EOM
+)
+
 # Fetch the current open issue that was created before.
 # Ideally this case should not happen since the issue either would have been closed after release or not created at all.
 # This is fail safe where some body rerun the Release Process workflow again then existing issue will be closed and new one will be opened
@@ -24,6 +42,6 @@ pre_release_tag=$(gh api "/repos/$REPO_OWNER/$PROJECT_REPONAME/releases" | jq --
 
 # Fetch name and body of the Pre Release tag
 issue_name=$(echo "$pre_release_tag" | jq -r '.name')
-issue_body=$(echo "$pre_release_tag" | jq -r '.body')
+issue_body=$(echo "$pre_release_tag" | jq -r '.body' | echo "$issue_steps")
 
 gh issue create --title "$issue_name" --body "$issue_body"
